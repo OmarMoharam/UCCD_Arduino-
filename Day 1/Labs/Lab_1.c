9f7e9db293d284f73eb4c1c3012ff4f9a5a05cce
OmarMoharam <omar.tarek8855@gmail.com>
2022-03-30 02:14:38 +0200

    Added Day 1

---
+++ Day 1/Labs/Lab_1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main() {
+
+    printf("I'm omar tarek\n\n"
+    "My birth date is 26 Dec 2001\n\n"
+    "I graduated from faculty of Engineering\n\n"
+    "Mansoura University 2013\n\n");
+}
---
+++ Day 1/Labs/Lab_2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main(){
+
+    int num;
+    printf("Please Enter the value: ");
+    scanf(" %d",&num);
+    printf("The value is %d",num);
+}
